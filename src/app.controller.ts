efdac3b411f506dbcf7c5a6f761014bff9e78e37
Kim Ariel Dequilla <deqkim4@gmail.com>
2024-07-11 20:20:40 +0800

    feat: upload/view images

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload/PortalDev')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      storage: diskStorage({
+        destination: 'public/Images/PortalDev',
+        filename: (req, file, cb) => {
+          cb(null, `${uuidv4()}.jpg`);
+        },
+      }),
+    }),
+  )
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    const file = files[0];
+    return {
+      path: `/${file.destination}/${file.filename}`,
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    index: false,
+    prefix: '/public',
+  });
+  await app.listen(7500);
 }
 bootstrap();
